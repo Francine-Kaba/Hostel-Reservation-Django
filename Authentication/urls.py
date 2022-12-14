42c51007332e9e3aaaba843575927c1e446b8aa0
Francine-Kaba <117295226+Francine-Kaba@users.noreply.github.com>
2022-12-14 08:12:58 +0000

    Made review changes

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -3,7 +3,6 @@ from Authentication.views import AddStudent, AddFaculty, AddAdmin, UserRoles
 
 urlpatterns = [
     path('add-user-role/', UserRoles.as_views(), name='add user roles'),
-    path('add-admin/', AddAdmin.as_view(), name='add admin'),
     path('add-faculty/', AddFaculty.as_view(), name='add faculty'),
     path('signup/', AddStudent.as_view(), name='add-student'),   
 ]
--- Authentication/views.py
+++ Authentication/views.py
@@ -18,64 +18,6 @@ class AddUserRoles(APIView):
         return JsonResponse({'message': 'User role added succesfully!', 'data': list(data)})
 
 """
-The AddAdmin class creates and adds a new admin into the database
-"""
-class AddAdmin(APIView):
-    def post(self, request, *args, **kwargs):
-        first_name = request.data.get('first name')
-        last_name = request.data.get('last name')
-        email = request.data.get('email')
-        password = request.data.get('password')
-        position = request.data.get('position')
-        role_id = request.data.get('student id')
-
-        try:
-            Admin.objects.get(email=email)
-            raise AuthenticationFailed("User already exists")
-        except Admin.DoesNotExist:
-            try:
-                UserRoles.objects.get(id=role_id)
-
-                details = {
-                    'first_name' : first_name,
-                    'last_name' : last_name,
-                    'role_id' : role_id,
-                    'email' : email,
-                    'position': position,
-                    'password' : make_password(password)
-                }
-                check_password = validate_password(password)   #password validation
-                if check_password["status"]:
-                    Admin.objects.create(**details)
-                    data = Admin.objects.filter().values('id', 'first_name', 'last_name', 'role_id', 'email', 'position', 'password')
-                    return JsonResponse({'message': 'Admin added succesfully', 'data': list(data)})
-            except:
-                return
-
-
-
-"""
-The AddFaculty class creates and adds a new faculty into the database
-"""
-class AddFaculty(APIView):
-    def post(self, request, *args, **kwargs):
-        name = request.data.get('name')
-        Faculty.objects.create(name=name)
-        data = Faculty.objects.filter().values('id', 'name')
-        return JsonResponse({'message': 'Faculty added succesfully!', 'data': list(data)})
-
-"""
-The AddProgram class creates and adds a new program into the database
-"""
-class AddProgram(APIView):
-    def post(self, request, *args, **kwargs):
-        name = request.data.get('name')
-        Program.objects.create(name=name)
-        data = Program.objects.filter().values('id', 'name')
-        return JsonResponse({'message': 'Program added succesfully!', 'data': list(data)})
-
-
-"""
 The AddStudent class creates and adds a new student into the database
 """
 class AddStudent(APIView):
@@ -106,12 +48,15 @@ class AddStudent(APIView):
                     'phone_number' : phone_number,
                     'faculty' : faculty,
                     'program' : program
-                }  
-                check_password = validate_password(password) # this code validates the password
-                if check_password["status"]:
-                    Student.objects.create(**details)  
-                    data = Student.objects.filter().values(**details)
-                    return JsonResponse({'message': 'Student added succesfully', 'data': list(data)})
+                }
+                try:
+                    check_password = validate_password(password) # this code validates the password
+                    if check_password["status"]:
+                        Student.objects.create(**details)  
+                        data = Student.objects.filter().values(**details)
+                        return JsonResponse({'message': 'Student added succesfully', 'data': list(data)})
+                except:
+                    raise AuthenticationFailed('Invalid password')
             except:
-                return
+                raise AuthenticationFailed('User already exists')
 
