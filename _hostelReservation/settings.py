3e583f4861b35dac16462b495f89c6ca1966699e
Francine-Kaba <117295226+Francine-Kaba@users.noreply.github.com>
2022-12-22 15:22:55 +0000

    Added the unit testing for the models and views

--- _hostelReservation/settings.py
+++ _hostelReservation/settings.py
@@ -82,13 +82,11 @@ AUTH_USER_MODEL = 'Authentication.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': os.getenv('DATABASE_NAME'),
-        # 'USER': os.getenv('DATABASE_USER'),
-        # 'PASSWORD': os.getenv('DATABASE_PASS'),
-        # 'HOST': os.getenv('DATABASE_HOST')
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASS'),
+        'HOST': os.getenv('DATABASE_HOST')
     }
 }
 
